5c6d17f1252d55fed8a8937738ffff377ef76202
WardQ <ward.quaghebeur@gmail.com>
2018-03-12 15:04:23 -0400

    added DiceGame

--- diceGame.py
+++ diceGame.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 def countWins(dice1, dice2):
     """
 
@@ -17,13 +18,14 @@ def countWins(dice1, dice2):
     dice1_wins, dice2_wins = 0, 0
 
     for i in dice1:
-      for j in dice2:
-        if i > j:
-          dice1_wins += 1
-        elif j > i:
-          dice2_wins += 1
+        for j in dice2:
+            if i > j:
+                dice1_wins += 1
+            elif j > i:
+                dice2_wins += 1
 
     return dice1_wins, dice2_wins
+
 
 def findBestDice(dices):
     """
@@ -42,17 +44,18 @@ def findBestDice(dices):
     assert all(len(dice) == 6 for dice in dices)
 
     for i in range(len(dices)):
-      wins = 0
-      for j in range(len(dices)):
-        dice1 = dices[i]
-        dice2 = dices[j]
-        a, b = countWins(dice1, dice2)
-        if a > b:
-          wins += 1
-      if wins == len(dices)-1:
-        return i
+        wins = 0
+        for j in range(len(dices)):
+            dice1 = dices[i]
+            dice2 = dices[j]
+            a, b = countWins(dice1, dice2)
+            if a > b:
+                wins += 1
+        if wins == len(dices) - 1:
+            return i
 
     return -1
+
 
 def findBestStrategy(dices):
     """
@@ -84,20 +87,21 @@ def findBestStrategy(dices):
     best_dice = findBestDice(dices)
 
     if best_dice == -1:
-      strategy["choose_first"] = False
-      for i in range(len(dices)):
-        for j in range(len(dices)):
-          dice1 = dices[i]
-          dice2 = dices[j]
-          a, b = countWins(dice1, dice2)
-          if a < b:
-            strategy[i] = j
+        strategy["choose_first"] = False
+        for i in range(len(dices)):
+            for j in range(len(dices)):
+                dice1 = dices[i]
+                dice2 = dices[j]
+                a, b = countWins(dice1, dice2)
+                if a < b:
+                    strategy[i] = j
 
     else:
-      strategy["choose_first"] = True
-      strategy["first_dice"] = best_dice
+        strategy["choose_first"] = True
+        strategy["first_dice"] = best_dice
 
     return strategy
+
 
 class TestUM(unittest.TestCase):
 
@@ -114,14 +118,18 @@ class TestUM(unittest.TestCase):
         self.assertEqual(countWins(dice1, dice2), (16, 20))
 
     def testFindBestDice(self):
-        self.assertEqual(findBestDice([[1, 1, 6, 6, 8, 8], [2, 2, 4, 4, 9, 9], [3, 3, 5, 5, 7, 7]]), -1)
-        self.assertEqual(findBestDice([[1, 1, 2, 4, 5, 7], [1, 2, 2, 3, 4, 7], [1, 2, 3, 4, 5, 6]]), 2)
-        self.assertEqual(findBestDice([[3, 3, 3, 3, 3, 3], [6, 6, 2, 2, 2, 2], [4, 4, 4, 4, 0, 0], [5, 5, 5, 1, 1, 1]]), -1)
+        self.assertEqual(findBestDice(
+            [[1, 1, 6, 6, 8, 8], [2, 2, 4, 4, 9, 9], [3, 3, 5, 5, 7, 7]]), -1)
+        self.assertEqual(findBestDice(
+            [[1, 1, 2, 4, 5, 7], [1, 2, 2, 3, 4, 7], [1, 2, 3, 4, 5, 6]]), 2)
+        self.assertEqual(findBestDice([[3, 3, 3, 3, 3, 3], [6, 6, 2, 2, 2, 2], [
+                         4, 4, 4, 4, 0, 0], [5, 5, 5, 1, 1, 1]]), -1)
 
     def testFindBestStrategy(self):
-        self.assertEqual(findBestStrategy([[1, 1, 4, 6, 7, 8], [2, 2, 2, 6, 7, 7], [3, 3, 3, 5, 5, 8]]), {'choose_first': False, 0: 1, 1: 2, 2: 0})
-        self.assertEqual(findBestStrategy([[4, 4, 4, 4, 0, 0], [7, 7, 3, 3, 3, 3], [6, 6, 2, 2, 2, 2], [5, 5, 5, 1, 1, 1]]), {'choose_first': True, 'first_dice': 1})
-
+        self.assertEqual(findBestStrategy([[1, 1, 4, 6, 7, 8], [2, 2, 2, 6, 7, 7], [
+                         3, 3, 3, 5, 5, 8]]), {'choose_first': False, 0: 1, 1: 2, 2: 0})
+        self.assertEqual(findBestStrategy([[4, 4, 4, 4, 0, 0], [7, 7, 3, 3, 3, 3], [
+                         6, 6, 2, 2, 2, 2], [5, 5, 5, 1, 1, 1]]), {'choose_first': True, 'first_dice': 1})
 
 
 if __name__ == '__main__':
